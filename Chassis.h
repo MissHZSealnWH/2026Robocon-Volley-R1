a77feaca16e5db89ec01cda59066784c30b9e3a4
CLLR <3228847917@qq.com>
2026-03-24 21:49:57 +0800

    添加了遥控器失联保护

--- Chassis.h
+++ Chassis.h
@@ -3,41 +3,43 @@
 
 #include "Task_Init.h"
 #include <stdbool.h>
-#include "PID.h"
+#include "PID_old.h"
 #include "VESC.h"
 
 #define PI 3.14159265359f
-#define MAX_VELOCITY 10.0f	  // 底盘最大速度
-#define MAX_OMEGA PI*10	 	 //最大角速度
+#define MAX_ROBOT_VEL 4.0f	  // 底盘最大速度
+#define MAX_ROBOT_OMEGA PI	 	 //最大角速度
 #define R 0.457f	 	//整车半径
 #define WHEEL_RADIUS 0.075f  //轮的半径
 
 
 //电机参数
 typedef struct{
-	PID_EREOR_TypeDef PID;
+	PID2 PID;
 	VESC_t steering;
-	uint8_t deadband;//死区控制（有效遏制电机抖动）
-}Motor_param;
 
+}Motor_param;
 
 typedef enum {
      STP,//自动模式下的急停
      STOP,//遥控模式下的急停
      REMOTE,
-	   CHOOSE,
-}Positon_label;
+	   AUTO,
+}Chassis_MODE;
 
 //任务
-extern TaskHandle_t Move_Remote_Handle;
+extern TaskHandle_t Remote_JY61_Handle;
 extern TaskHandle_t Remote_Handle;
 
 //模式
-extern Positon_label MODE;
+extern Chassis_MODE MODE;
 
 //任务函数
 void Remote(void *pvParameters);
-void Move_Remote(void *pvParameters);
+void Remote_JY61(void *pvParameters);
 
-bool is_remote_active(void);
+void Remote_Analysis();
+
+#define KEY_RISING_EDGE(cur, last, field)  ((cur.field == 1) && (last.field == 0))
+
 #endif
